31ebdcfd35b11422ec3cd1e99bcec28097eeeda4
JGarcia575 <jimenagarcia575@gmail.com>
2023-11-07 18:07:26 -0300

    añadida la funcionalidad jwt

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const router = require('./routes/userRoutes');
 const bodyParse = require('body-parser');
+const authRouter = require('./routes/authRoutes');
 
 const port = 3000;
 
@@ -10,6 +12,7 @@ app.use(bodyParse.json());
 
 //Rutas
 app.use(router);
+app.use(authRouter);
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);

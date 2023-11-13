8cce058e20021757725e856b31b946a37bf8fba8
TheLazyCat <117136042+JGarcia575@users.noreply.github.com>
2023-11-13 01:36:56 -0300

    Revert "Feature/sequelize"

--- src/routes/fileUpload.js
+++
@@ -1,9 +0,0 @@
-const fileRouter = require('express').Router();
-const {
-    subirArchivo
-} = require('./../controllers/fileUpload');
-
-
-fileRouter.post('/upload', subirArchivo);
-
-module.exports = fileRouter;
